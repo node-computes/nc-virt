dd9bf056090d19962c19862460b603fde44f9576
Thibaut Lapierre <thibaut@hp.com>
2016-01-13 18:11:31 +0000

    Work ongoing

--- virt.js
+++ virt.js
@@ -1,48 +1,47 @@
-'use strict';
 
 var libvirt = require('libvirt'),
     Hypervisor = libvirt.Hypervisor,
     h = require('./helpers/helper');
 
-var compute = {};
-
-var version = h.getLibVirtVersion().then(function(version) {
+h.getLibVirtVersion().then(function(version) {
     console.log('LibVirt Version: ' + version);
 });
 
+var connection = new Hypervisor('qemu:///system');
+connection.connect(function() {
 
-compute.hypervisor = new Hypervisor('qemu:///system');
-console.log('URI: ' + compute.hypervisor.uri);
+    const hostname = connection.getHostname();
+    console.log(hostname);
 
-compute.hypervisor.connect();
+    connection.lookupDomainById(24, function(err, domain) {
+        //console.log(err);
+        var domain = domain;
+        console.log(domain);
+        //console.log(domain._parent);
+    });
 
-compute.type = compute.hypervisor.getHostname();
-compute.hypervisor.lookupDomainById(24, function(err, domain) {
-    //console.log(err);
-    console.log(domain);
-    //console.log(domain._parent);
+    console.log('Type: ' + connection.type);
+    console.log('Hostname: ' + connection.getHostname());
+    console.log('VM List: ' + connection.vm_list);
+
+    var capabilities = connection.getCapabilities();
+    console.log('Capabilities: ' + capabilities);
+
+    connection.vm_list = connection.getAllDomains().then(function () {
+            var self = this;
+            return Promise.join([ this.listDefinedDomainsAsync(), this.listActiveDomainsAsync()
+                ])
+                .spread(function(defined, active) { return defined.concat(active); })
+                .spread(function(defined, active) {
+                    return Promise.all([
+                        Promise.map(defined, function(domain) { return self.lookupDomainByNameAsync(domain); }),
+                        Promise.map(active, function(domain) { return self.lookupDomainByIdAsync(domain); })
+                    ]);
+                })
+                .spread(function(defined, active) { return defined.concat(active); });
+        }
+    );
+
 });
 
-compute.vm_list = compute.hypervisor.getAllDomains().then(function () {
-        var self = this;
-        return Promise.join([ this.listDefinedDomainsAsync(), this.listActiveDomainsAsync()
-            ])
-            .spread(function(defined, active) { return defined.concat(active); })
-            .spread(function(defined, active) {
-                return Promise.all([
-                    Promise.map(defined, function(domain) { return self.lookupDomainByNameAsync(domain); }),
-                    Promise.map(active, function(domain) { return self.lookupDomainByIdAsync(domain); })
-                ]);
-            })
-            .spread(function(defined, active) { return defined.concat(active); });
-    }
-);
-console.log('Type: ' + compute.type);
-console.log('Hostname: ' + compute.hypervisor.getHostname());
-console.log('VM List: ' + compute.vm_list);
-
-var capabilities = compute.hypervisor.getCapabilities();
-console.log('Capabilities: ' + capabilities);
-
-
-compute.hypervisor.disconnect();
+connection.disconnect();
