6385f92bdffa3276e6b346cd8ff9707f00a27aa8
Thibaut Lapierre <thibaut@hp.com>
2016-01-14 11:23:27 +0000

    Display list

--- virt.js
+++ virt.js
@@ -4,43 +4,108 @@ var libvirt = require('libvirt'),
     h = require('./helpers/helper');
 
 h.getLibVirtVersion().then(function(version) {
-    console.log('LibVirt Version: ' + version);
+    //console.log('LibVirt Version: ' + version);
 });
 
 var connection = new Hypervisor('qemu:///system');
 connection.connect(function() {
 
-    const hostname = connection.getHostname();
-    console.log(hostname);
-
-    connection.lookupDomainById(24, function(err, domain) {
-        //console.log(err);
-        var domain = domain;
-        console.log(domain);
-        //console.log(domain._parent);
+    connection.getHostname(function(err, result) {
+        //console.log('Hostname: ' + result);
+    });
+    connection.getCapabilities(function(err, result) {
+        //console.log(result);
     });
 
-    console.log('Type: ' + connection.type);
-    console.log('Hostname: ' + connection.getHostname());
-    console.log('VM List: ' + connection.vm_list);
+    connection.lookupDomainById(24, function(err, domain) {
+        //console.log(domain);
+    });
 
-    var capabilities = connection.getCapabilities();
-    console.log('Capabilities: ' + capabilities);
+    connection.getSysInfo(function(err, result) {
+        //console.log('SysInfo: ' + result);
+    });
 
-    connection.vm_list = connection.getAllDomains().then(function () {
-            var self = this;
-            return Promise.join([ this.listDefinedDomainsAsync(), this.listActiveDomainsAsync()
-                ])
-                .spread(function(defined, active) { return defined.concat(active); })
-                .spread(function(defined, active) {
-                    return Promise.all([
-                        Promise.map(defined, function(domain) { return self.lookupDomainByNameAsync(domain); }),
-                        Promise.map(active, function(domain) { return self.lookupDomainByIdAsync(domain); })
-                    ]);
-                })
-                .spread(function(defined, active) { return defined.concat(active); });
+//    connection.vm_list = connection.getAllDomains().then(function () {
+//            var self = this;
+//            return Promise.join([ this.listDefinedDomainsAsync(), this.listActiveDomainsAsync()
+//                ])
+//                .spread(function(defined, active) { return defined.concat(active); })
+//                .spread(function(defined, active) {
+//                    return Promise.all([
+//                        Promise.map(defined, function(domain) { return self.lookupDomainByNameAsync(domain); }),
+//                        Promise.map(active, function(domain) { return self.lookupDomainByIdAsync(domain); })
+//                    ]);
+//                })
+//                .spread(function(defined, active) { return defined.concat(active); });
+//        }
+//    );
+//    console.log('VM List: ' + connection.vm_list);
+
+    [
+        //{ name: 'capabilities', method: 'getCapabilities' },
+        { name: 'host name', method: 'getHostname' },
+        { name: 'type', method: 'getType' },
+        { name: 'connection uri', method: 'getConnectionUri' },
+        { name: 'libvirt version', method: 'getLibVirtVersion' },
+        { name: 'version', method: 'getVersion' },
+        { name: 'connection secure', method: 'isConnectionSecure' },
+        { name: 'connection encrypted', method: 'isConnectionEncrypted' },
+        { name: 'connection alive', method: 'isConnectionAlive'},
+        //{ name: 'system info', method: 'getSysInfo'},
+    ].forEach(function(attribute) {
+            connection[attribute.method](function(err, result) {
+                console.log(result);
+            });
+    });
+
+
+
+    [
+        {
+            name: 'defined but inactive domains',
+            method: 'listDefinedDomains', expected: []
+        },
+        {
+            name: 'defined but inactive networks',
+            method: 'listDefinedNetworks', expected: []
+        },
+        {
+            name: 'defined but inactive storage pools',
+            method: 'listDefinedStoragePools',
+            expected: []
+        },
+        {
+            name: 'defined but inactive interfaces',
+            method: 'listDefinedInterfaces', expected: []
+        },
+        {
+            name: 'active physical host interfaces',
+            method: 'listActiveInterfaces', expected: ['eth1']
+        },
+        {
+            name: 'active networks',
+            method: 'listActiveNetworks', expected: ['default']
+        },
+        {
+            name: 'active storage pools',
+            method: 'listActiveStoragePools', expected: ['default-pool']
+        },
+        {
+            name: 'network filters',
+            method: 'listNetworkFilters', disabled: true
+        },
+        {
+            name: 'defined secrets (uuids)',
+            method: 'listSecrets', disabled: true
         }
-    );
+
+    ].forEach(function(testCase) {
+            connection[testCase.method](function(err, result) {
+               console.log(testCase, result)
+            });
+    });
+
+
 
 });
 
